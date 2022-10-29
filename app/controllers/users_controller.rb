95f83c374e2a9dd4d1f5173c284b4de70b9ee94c
ikukee <rmpanis2@gmail.com>
2022-10-29 18:52:28 +0800

    Added encryption upon register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 	 
 	def create
 		@user = User.new(user_params)
-
+		@user.password = BCrypt::Password.create(@user.password)
 		respond_to do |format|
 			if @user.save
 			  format.html { redirect_to '/login', notice: "User was successfully created." }
