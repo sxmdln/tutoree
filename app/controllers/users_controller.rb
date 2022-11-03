64d7c0d4ca2b607d09912098820c237a2d797686
ikukee <rmpanis2@gmail.com>
2022-11-03 22:05:27 +0800

    Revert "configured user_controller"
    This reverts commit 0a1a4bab2c697a19589fcf7af89646bdacd26999.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 	def index 
-		puts Tutee.all
 	end
 	def login
 	end
@@ -10,29 +9,25 @@ class UsersController < ApplicationController
         	return true
         end
         if user && user.authenticate(params[:password])
-			redirect_to '/login', notice: "success!"
+            render json: {msg: 'Success', user: user}, status: :ok
         else
-            redirect_to '/login', notice: "fail!"
+            render json: {msg: 'Incorrect Password', user: user.password_digest }, status: :unprocessable_entity
         end
 
 	end
 	def new
 		@user = User.new
-		puts @user.tutee
 	end
 	 
 	def create
 		@user = User.new(user_params)
-
 		@user.password_digest = BCrypt::Password.create(@user.password)
-		
 		respond_to do |format|
-			if @user.save 
-				tutee = Tutee.create(_id: @user._id, user_id: @user.email_address)	
-				format.html { redirect_to '/login', notice: "User was successfully created."}
+			if @user.save
+			  format.html { redirect_to '/login', notice: "User was successfully created." }
 			else
-			  	format.html { render :new, status: :unprocessable_entity }
-			  	format.json { render json: @user.errors, status: :unprocessable_entity }
+			  format.html { render :new, status: :unprocessable_entity }
+			  format.json { render json: @user.errors, status: :unprocessable_entity }
 			end
 		end
 	end
@@ -42,6 +37,6 @@ class UsersController < ApplicationController
 		end
 
 		def user_params
-			params.require(:user).permit(:email_address, :password, :contact_number, :type)
+			params.require(:user).permit(:email_address, :password)
 		end
 end
