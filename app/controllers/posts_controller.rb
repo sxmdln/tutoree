4405b326fc3312eb59e06304b30985f51a19ec56
sxmdln <57649092+sxmdln@users.noreply.github.com>
2022-11-24 13:47:23 +0800

    Post controller (under construction)
    posts controller changes
    - def new
    - def create_post
    - def apply

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,17 +6,19 @@ class PostsController < ApplicationController
 
     def show
         @post = Post.find(params[:id])
-        @users = @post.users
     end
 
     def new
         @post = Post.new
+        @accepted_tutee = AcceptedTutee.new
     end
 
     def create_post
-		@post = Post.new(post_params, user_id: @post.user._id)
-        @post.is_accepted = false;
-
+		@post = Post.new(post_params)
+        @post.is_accepted = false
+        @user = User.find(session[:user_id])
+        @post.author_id = @user._id
+        
         respond_to do |format|
             if @post.save
                 format.html { redirect_to "/dashboard", notice: "Post was successfully created." }
@@ -30,16 +32,30 @@ class PostsController < ApplicationController
 
     def apply
         apply_post = Post.find(params[:id])
+        @accepted_tutee = AcceptedTutee.new(accepted_tutee_params)
 
         if apply_post.is_accepted == false then
-            apply_post.is_accepted = true;
+            apply_post.is_accepted = true
+            @accepted_tutee.tutee_id = apply_post.author_id
+            # @accepted_tutee.name = apply_post.post_name
+            # @accepted_tutee.price = apply_post.post_price
         end
         apply_post.save
-        redirect_to "/dashboard"
+        if @accepted_tutee.save
+                format.html { redirect_to "/dashboard", notice: "Accepted Tutee success." }
+                # format.json { render :show, status: :created, location: @post }
+        else
+                format.html { render :new, status: :unprocessable_entity }
+                # format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
     end
 
     private
         def post_params
-            params.require(:post).permit(:post_name, :post_price, :skill_category, :is_accepted, :date_posted)
+            params.require(:post).permit(:author_id, :post_name, :post_price, :skill_category, :is_accepted, :date_posted)
+        end
+
+        def accepted_tutee_params
+            params.require(:accepted_tutee).permit(:tutee_id,:name,:price)
         end
 end
